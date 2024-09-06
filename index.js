cb2ec8a9927c93a73ddcceb064f5af93aa139396
Junior Teixeira <teixeirajunior19051988@gmail.com>
2024-09-06 16:39:05 -0300

    Adição de mais uma lógica para o currentItem

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ controls.forEach(control => control.addEventListener('click', function() {
     
     if(control.classList.contains('is-right')){
         currentItem++
+        if(currentItem >= cards.length){
+            currentItem = 0
+        }
     }else{
         currentItem > 0 && currentItem--
+        if(currentItem === 0){
+            currentItem = cards.length - 1
+        }
     }
 
     cards[currentItem].classList.remove('off-focus')
