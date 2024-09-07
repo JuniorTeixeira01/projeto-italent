fb991c5e1b6ab619db59f5a104594ae737d03ce7
Junior Teixeira <teixeirajunior19051988@gmail.com>
2024-09-07 01:28:14 -0300

    Alterações do projeto e termino

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import preenchDiv  from "./data.js";
+import preenchDiv  from "./dados/data.js";
 
 
 preenchDiv();
@@ -6,24 +6,30 @@ preenchDiv();
 
 let currentItem = 0;
 
-const cards = document.querySelectorAll('.card');
+let cards = document.querySelectorAll('.card');
+let maxCard = cards.length;
+
+
 const controls = document.querySelectorAll('button');
 
 controls.forEach(control => control.addEventListener('click', function() {
     
-    if(control.classList.contains('is-right')){
-        currentItem++
-        if(currentItem >= cards.length){
-            currentItem = 0
-        }
-    }else{
-        currentItem > 0 && currentItem--
-        if(currentItem === 0){
-            currentItem = cards.length - 1
-        }
-    }
+    const isLeft = control.classList.contains('is-left')
 
-    cards[currentItem].classList.remove('off-focus')
 
-    console.log(currentItem)
+        isLeft ? currentItem -- : currentItem ++
+
+        if(currentItem >= maxCard){
+            currentItem = 0;
+        }
+        if(currentItem < 0){
+            currentItem = maxCard - 1
+        }
+
+
+        cards.forEach(card => card.classList.remove('off-focus'));
+
+        cards[currentItem].scrollIntoView({inline: "center", behavior: "smooth"})
+        
+        cards[currentItem].classList.add('off-focus');
 }))
